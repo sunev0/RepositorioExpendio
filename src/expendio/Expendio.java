e5314d1ad74586171b7d5ba78c58b6eacef36253
Nicole <Nicole@DESKTOP-AP2GI5P>
2021-05-06 17:57:36 -0600

    Prueba

--- src/expendio/Expendio.java
+++ src/expendio/Expendio.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package expendio;
 
 /**
@@ -11,11 +7,9 @@ package expendio;
  */
 public class Expendio {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Holi");
     }
     
 }
